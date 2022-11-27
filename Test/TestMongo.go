c3a56f9daaa2b4e3c34498f7ac71028c45c78e58
huluhutu <842830066@qq.com>
2022-11-27 12:44:37 +0800

    test

--- Test/TestMongo.go
+++ Test/TestMongo.go
@@ -4,6 +4,7 @@ import (
 	"boboshu/config"
 	"context"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	_ "gorm.io/gorm/clause"
 	"log"
 )
@@ -40,4 +41,45 @@ func InsertMany(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(200, user2)
+}
+
+func UpdateOne(ctx *gin.Context) {
+	db := config.ConnectToMongoDB()
+	defer config.DisConnectMongo(db)
+	userCollection := db.Database("test").Collection("users")
+	filter := bson.D{{"name", "Lance"}}
+	update := bson.D{{"$set", bson.D{{"name", "HULUHUTU"}}}}
+	_, err := userCollection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	ctx.JSON(200, "更新成功")
+}
+
+func FindOne(ctx *gin.Context) {
+	db := config.ConnectToMongoDB()
+	defer config.DisConnectMongo(db)
+	userCollection := db.Database("test").Collection("users")
+	filter := bson.D{{"name", "Lance"}}
+	var user3 User
+	err := userCollection.FindOne(context.TODO(), filter).Decode(&user3)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	ctx.JSON(200, user3)
+}
+
+func DeleteOne(ctx *gin.Context) {
+	db := config.ConnectToMongoDB()
+	defer config.DisConnectMongo(db)
+	userCollection := db.Database("test").Collection("users")
+	filter := bson.D{{"name", "Dean"}}
+	one, err := userCollection.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	ctx.JSON(200, one)
 }
--- router/TestRouter.go
+++ router/TestRouter.go
@@ -7,6 +7,8 @@ func init() {
 	{
 		testGroup.GET("/mongo/insertOne", Test.InsetOne)
 		testGroup.GET("/mongo/insertMany", Test.InsertMany)
-
+		testGroup.GET("/mongo/updateOne", Test.UpdateOne)
+		testGroup.GET("/mongo/findOne", Test.FindOne)
+		testGroup.GET("/mongo/deleteOne", Test.DeleteOne)
 	}
 }
